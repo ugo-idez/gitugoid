9fd5af56df0850057a1789ba99df557fefd8e6b1
Ugo IDEZ <93016029+ugo-idez@users.noreply.github.com>
2022-03-17 11:50:21 +0100

    Create Ad.ps1

---
+++ script-pwsh/Ad.ps1
@@ -0,0 +1,18 @@
+#
+# Script Windows PowerShell pour le déploiement d’AD DS
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "gsb.lan" `
+-DomainNetbiosName "GSB" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
